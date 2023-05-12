6f0ae43664da6d6b6e7545d9eb249cf4a434cc44
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-05-12 14:24:02 -0300

    css-corrigido

--- cep.js
+++ cep.js
@@ -15,7 +15,7 @@ if (!("erro" in conteudo)) {
     document.getElementById('bairro').value=(conteudo.bairro);
     document.getElementById('cidade').value=(conteudo.localidade);
     document.getElementById('uf').value=(conteudo.uf);
-    document.getElementById('ibge').value=(conteudo.ibge);
+    // document.getElementById('ibge').value=(conteudo.ibge);
 } //end if.
 else {
     //CEP não Encontrado.
@@ -43,7 +43,7 @@ if (cep != "") {
         document.getElementById('bairro').value="...";
         document.getElementById('cidade').value="...";
         document.getElementById('uf').value="...";
-        document.getElementById('ibge').value="...";
+        // document.getElementById('ibge').value="...";
 
         //Cria um elemento javascript.
         var script = document.createElement('script');
